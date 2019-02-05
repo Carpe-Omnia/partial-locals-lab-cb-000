44ae06ed78f511c79b75b092502c1035cb4d4293
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-02-05 21:28:47 +0000

    Automatically backed up by Learn

--- app/controllers/classrooms_controller.rb
+++ app/controllers/classrooms_controller.rb
@@ -1,7 +1,7 @@
 class ClassroomsController < ApplicationController
   def show
     @classroom = Classroom.find(params[:id])
-    @students = @classroom.students
+    @students = Classroom.find(params[:id]).students
   end
 
   def index
