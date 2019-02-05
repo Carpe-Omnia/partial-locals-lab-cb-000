ce9b719071dba543f34748577ed63537dbdcde7a
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-02-05 21:37:08 +0000

    Automatically backed up by Learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -13,4 +13,11 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+  def self.search(name)
+    self.all.collect do |student|
+      student.name.upcase.match(/#{name.upcase}/)
+    end
+  end
+
 end
